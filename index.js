2f4e0e00b09bbf6cebca329c0a5706b340d0d7cc
Kunal Bagaria <kb24x7@gmail.com>
2021-03-29 00:02:38 +0530

    fix SORS

--- index.js
+++ index.js
@@ -21,6 +21,11 @@ app.use(bParser.urlencoded({ extended: true }))
 app.use(bParser.json());
 
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use(express.static("frontend/build"));
   app.get("/", (req, res) => {
